ae1afc3ad6ddf0558251928f0eaef42b01d4707e
JohnMeadow <leszek.nowak@uj.edu.pl>
2018-07-21 03:29:34 +0200

    shroom blink

--- source/objects/shroom.gd
+++ source/objects/shroom.gd
@@ -1,13 +1,31 @@
 extends MeshInstance
 
-# class member variables go here, for example
+var pickable = false
+var blink_value = 1
+var blink_iterattor = 1
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _physics_process(delta):
+	if pickable:
+		blink_value -= delta * blink_iterattor
+		if blink_value <= 0.5 || blink_value >= 1:
+			blink_iterattor *=-1
+			blink_value -= delta * blink_iterattor
+		self.material_override.set("albedo_color", Color(blink_value,blink_value,blink_value))
+#		set("albedo_color", Color(blink_value,blink_value,blink_value) )
+#		print(Color(blink_value,blink_value,blink_value))
+
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("players"):
+		pickable = true
+
+
+func _on_Area_body_exited(body):
+	if body.is_in_group("players"):
+		self.material_override.set("albedo_color", Color(1,1,1) )
+		pickable = false
--- source/objects/tree.gd
+++ source/objects/tree.gd
@@ -5,7 +5,7 @@ var shroom_object = load("res://objects/shroom.tscn")
 func _ready():
 	randomize()
 
-	for i in range( max(0, randi() % 10 - 5) ):
+	for i in range( max(0, randi() % 10 ) ):
 		spawn_shroom()
 
 	
@@ -18,7 +18,7 @@ func spawn_shroom():
 	new_shroom.translation = translation 
 	new_shroom.translation.x += rand_range(1,5) * (( randi() % 2 ) * 2 - 1)
 	new_shroom.translation.z += rand_range(1,5) * (( randi() % 2 ) * 2 - 1)
-
+	new_shroom.material_override = SpatialMaterial.new()
 	$Shrooms.add_child( new_shroom )
 
 func grow_shroom():
