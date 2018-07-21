3426aab181e57da064fa98973a52f06d351ecbd7
Tomasz Szymański <cl.arctos.orion@gmail.com>
2018-07-21 02:56:40 +0200

    goat attack!

--- source/enemies/goat.gd
+++ source/enemies/goat.gd
@@ -2,16 +2,17 @@ extends KinematicBody
 
 export(int) var RANGE
 
-const MOVE_BALANCE = 2.5
+const MOVE_BALANCE = 5
 
 const MOVE_SPEED_WALK  = 10 * MOVE_BALANCE
 const MOVE_SPEED_CHASE = 50 * MOVE_BALANCE
-const MOVE_SPEED_RUN   = 100 * MOVE_BALANCE
+const MOVE_SPEED_RUN   = 10 * MOVE_BALANCE
 
-enum {STATE_IDLE, STATE_WALK, STATE_CHASE}
+const RUN_DISTANCE     = 5
 
-var move       = Vector3()
-var walk_cycle = 0
+enum {STATE_IDLE, STATE_BACK, STATE_CHASE}
+
+var state = null
 
 #var playerInArea = false
 var originPosition = Vector3()
@@ -19,6 +20,7 @@ var target = null
 
 func _ready():
 	originPosition = self.translation
+	state = STATE_IDLE
 	
 	pass
 
@@ -27,22 +29,38 @@ func _process(delta):
 	var offset = 0
 #	var player_moved = false
 	
-	if (target != null && self.translation.distance_to(target.translation) > 0.5):
-		offset = MOVE_SPEED_CHASE * delta
-#		print("Chase: " + target.get_name())
+	if target && state == STATE_CHASE:
+		var distance = self.translation.distance_to(target.translation)
 		
 		var direction = target.translation - self.translation
 		direction = direction.normalized()
 		
 		look_at(target.translation, Vector3(0,1,0))
-		move_and_slide(direction * offset)
+		
+		if distance > RUN_DISTANCE:
+			offset = MOVE_SPEED_CHASE * delta
+			move_and_slide(direction * offset)
+		else:
+			offset = MOVE_SPEED_RUN * delta
+			var collision = move_and_collide(direction * offset)
+			if collision && collision.get_collider().get_name() == target.get_name():
+				target.push(direction)
+				state = STATE_BACK
+				
+				
+				
+#		print("Chase: " + target.get_name())
+		
+		
 	pass
 
 
 func _on_SenseArea_body_entered(body):
-	if("player" in body.get_name()):
+	if "player" in body.get_name():
 		target = body
+		state = STATE_CHASE
 
 
 func _on_SenseArea_body_exited(body):
 	target = null
+	state = STATE_IDLE
