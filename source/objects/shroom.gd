0f96aaac192d57c42f2a4d6dd009ccf1161c1663
JohnMeadow <leszek.nowak@uj.edu.pl>
2018-07-21 23:03:05 +0200

    Glowing mushrooms

--- source/objects/shroom.gd
+++ source/objects/shroom.gd
@@ -7,18 +7,23 @@ var size            = 0
 var max_size        = 20
 var growth_rate     = 0.01
 
+var shroom_material = [preload("res://objects/Textures/shroom1.material"),preload("res://objects/Textures/shroom2.material") ]
+
 func _ready():
+	rotation.y = rand_range(0,360)
+	$MeshInstance.material_override = shroom_material[randi()%2]
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
 func _physics_process(delta):
 	if pickable:
-		blink_value -= delta * blink_iterattor
-		if blink_value <= 0.5 || blink_value >= 1:
-			blink_iterattor *= -1
-			blink_value     -= delta * blink_iterattor
-		$MeshInstance.material_override.set("albedo_color", Color(blink_value,blink_value,blink_value))
+#		blink_value -= delta * blink_iterattor
+#		if blink_value <= 0.5 || blink_value >= 1:
+#			blink_iterattor *= -1
+#			blink_value     -= delta * blink_iterattor
+#		$MeshInstance.material_override.set("albedo_color", Color(blink_value,blink_value,blink_value))
+		pass
 	elif size < max_size:
 		size += growth_rate
 		$MeshInstance.scale = Vector3(size,size,size)
@@ -40,5 +45,5 @@ func _on_Area_body_exited(body):
 		if is_in_group("pickables1") || is_in_group("pickables2") || is_in_group("pickables3") || is_in_group("pickables4"):
 			pass
 		else: 
-			$MeshInstance.material_override.set("albedo_color", Color(1,1,1) )
+#			$MeshInstance.material_override.set("albedo_color", Color(1,1,1) )
 			pickable = false
