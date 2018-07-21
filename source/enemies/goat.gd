34fd5811bc660bfcdd957184d9083168e9fe9136
Tomasz Szymański <cl.arctos.orion@gmail.com>
2018-07-21 03:10:37 +0200

    Goat AI!

--- source/enemies/goat.gd
+++ source/enemies/goat.gd
@@ -12,7 +12,10 @@ const RUN_DISTANCE     = 5
 
 enum {STATE_IDLE, STATE_BACK, STATE_CHASE}
 
+const WAIT_TIME = 3
+
 var state = null
+var time = 0
 
 #var playerInArea = false
 var originPosition = Vector3()
@@ -20,7 +23,7 @@ var target = null
 
 func _ready():
 	originPosition = self.translation
-	state = STATE_IDLE
+	state = STATE_BACK
 	
 	pass
 
@@ -45,12 +48,21 @@ func _process(delta):
 			var collision = move_and_collide(direction * offset)
 			if collision && collision.get_collider().get_name() == target.get_name():
 				target.push(direction)
-				state = STATE_BACK
-				
-				
-				
-#		print("Chase: " + target.get_name())
-		
+				state = STATE_IDLE
+				time = 0;
+	elif target && state == STATE_IDLE && time < WAIT_TIME:
+		time += delta;
+	elif target && state == STATE_IDLE && time >= WAIT_TIME:
+		state = STATE_CHASE
+		time = 0;
+	elif !target && state == STATE_IDLE:
+		state = STATE_BACK
+	elif state == STATE_BACK:
+		var direction = originPosition - self.translation
+		direction = direction.normalized()
+		look_at(originPosition, Vector3(0,1,0))
+		offset = MOVE_SPEED_WALK * delta
+		move_and_slide(direction * offset)
 		
 	pass
 
@@ -63,4 +75,4 @@ func _on_SenseArea_body_entered(body):
 
 func _on_SenseArea_body_exited(body):
 	target = null
-	state = STATE_IDLE
+	state = STATE_BACK
