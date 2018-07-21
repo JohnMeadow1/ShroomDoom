9eab3345bee492d55f40a43d3aece9d1e6b520df
Tomasz Szymański <cl.arctos.orion@gmail.com>
2018-07-21 12:09:06 +0200

    Goat jumps!

--- source/enemies/goat.gd
+++ source/enemies/goat.gd
@@ -14,8 +14,11 @@ enum {STATE_IDLE, STATE_BACK, STATE_CHASE}
 
 const WAIT_TIME = 3
 
+var offset = 0
 var state = null
 var time = 0
+var player_moved = false
+var walk_cycle = 0
 
 #var playerInArea = false
 var originPosition = Vector3()
@@ -29,41 +32,17 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	
-	var offset = 0
-#	var player_moved = false
-	
 	if state == STATE_IDLE:
 		idle(delta)
+		player_moved = false
 	elif state == STATE_CHASE:
-		var distance = self.translation.distance_to(target.translation)
-		
-		var direction = target.translation - self.translation
-		direction = direction.normalized()
-		
-		look_at(target.translation, Vector3(0,1,0))
-		
-		if distance > RUN_DISTANCE:
-			offset = MOVE_SPEED_CHASE * delta
-			move_and_slide(direction * offset)
-		else:
-			offset = MOVE_SPEED_RUN * delta
-			var collision = move_and_collide(direction * offset)
-			if collision && collision.get_collider().get_name() == target.get_name():
-				target.push(direction)
-				state = STATE_IDLE
-				time = 0;
-				if targets.size() > 1:
-					targets.push_back(target)
-					targets.pop_front()
-					target = targets.front()
+		chase(delta)
+		player_moved = true
 	elif state == STATE_BACK:
-		var direction = originPosition - self.translation
-		if direction.length_squared() >1:
-			direction = direction.normalized()
-			look_at(originPosition, Vector3(0,1,0))
-			offset = MOVE_SPEED_WALK * delta
-			move_and_slide(direction * offset)
+		back(delta)
+		player_moved = true
+		
+	bounce(player_moved)
 
 func idle(delta):
 	if time < WAIT_TIME:
@@ -71,6 +50,54 @@ func idle(delta):
 	else:
 		state = STATE_CHASE
 		time = 0;
+
+func chase(delta):
+	var distance = self.translation.distance_to(target.translation)
+		
+	var direction = target.translation - self.translation
+	direction = direction.normalized()
+	
+	look_at(target.translation, Vector3(0,1,0))
+	
+	if distance > RUN_DISTANCE:
+		offset = MOVE_SPEED_CHASE * delta
+		move_and_slide(direction * offset)
+	else:
+		offset = MOVE_SPEED_RUN * delta
+		var collision = move_and_collide(direction * offset)
+		if collision && collision.get_collider().get_name() == target.get_name():
+			target.push(direction)
+			state = STATE_IDLE
+			time = 0;
+			if targets.size() > 1:
+				targets.push_back(target)
+				targets.pop_front()
+				target = targets.front()
+
+func back(delta):
+	var direction = originPosition - self.translation
+	
+	if direction.length_squared() >1:
+		direction = direction.normalized()
+		look_at(originPosition, Vector3(0,1,0))
+		offset = MOVE_SPEED_WALK * delta
+		move_and_slide(direction * offset)
+
+func bounce(player_moved):
+	if player_moved:
+		walk_cycle += 0.2
+		if walk_cycle >= PI:
+			walk_cycle -= PI
+	else:
+		if walk_cycle > PI * 0.5:
+			walk_cycle += 0.2
+		else:
+			walk_cycle -= 0.2
+			
+		if walk_cycle < 0 or walk_cycle > PI:
+				walk_cycle = 0
+	
+	$Spatial.translation.y =  -sin( walk_cycle ) * 0.2
 
 func _on_SenseArea_body_entered(body):
 	if body.is_in_group("players"):
