c713da9149da8c9744473bbd9ea33f158eb8d148
Tomasz Szymański <cl.arctos.orion@gmail.com>
2018-07-22 02:00:10 +0200

    Add final flash

--- source/globals.gd
+++ source/globals.gd
@@ -1,14 +1,29 @@
 extends Node
 
+const BLINDER_TIME = 3
+
 var player_score = [0,0,0,0]
 var player_score_label = [null,null,null,null]
+var blinder = null
+var won = false
+var timer = 3
 
 func _ready():
 	pass
-	
+
+func _physics_process(delta):
+	if won && blinder.color.a > 0:
+		if timer <= 0:
+			blinder.color.a -= delta / 4
+		else:
+			timer -= delta
+
 func add_score(player_id, value):
 	player_id-=1 # because array is from 0-3, and players are from 1-4
 	player_score[player_id] += value
 	player_score_label[player_id].text = "shrooms: "+str(player_score[player_id])
 	
-	
+func win(player):
+	blinder.color.a = 1
+	timer = BLINDER_TIME
+	won = true
