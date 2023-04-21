ea3a546748bee48c567d14ffd8d1a56a552d786b
John Meadow <leszek.nowak@uj.edu.pl>
2023-04-21 14:01:56 +0200

    Updated fiunal screen

--- source/scenes/victory_screen/final.gd
+++ source/scenes/victory_screen/final.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 var tempBG     = Vector2() 
 var tempShroom = Vector2() 
@@ -9,26 +9,27 @@ var timer      = 8
 
 func _ready():
 	scale = get_viewport().size / globals.screen
-	$Control.text += globals.victory_label +" player won?"
+	$Label.text += globals.victory_label +" player won?"
+	logging.save_logs()
 
 func _physics_process(delta):
 	
 	if timer<=0:
-		get_tree().change_scene_to_file("res://start.tscn")
+		get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
 	else:
 		timer -= delta
-	$BG.position -= tempBG
+	$background.position -= tempBG
 	tempBG = Vector2(randf_range(-timer/8,timer/8)*randf_range(-timer/8,timer/8),randf_range(-timer/8,timer/8)*randf_range(-timer/8,timer/8))
-	$BG.position += Vector2(0,-0.11*timer) + tempBG
+	$background.position += Vector2(0,-0.11*timer) + tempBG
 	
 	$shroom.position -= tempShroom
 	tempShroom = Vector2(randf_range(-timer/6,timer/6)*randf_range(-timer/6,timer/6),randf_range(-timer/6,timer/6)*randf_range(-timer/6,timer/6))
 	$shroom.position += Vector2(0,-0.14*timer) + tempShroom
 
-	$backCloud.position -= tempBack
+	$back_cloud.position -= tempBack
 	tempBack = Vector2(randf_range(-timer/5,timer/5)*randf_range(-timer/5,timer/5),randf_range(-timer/5,timer/5)*randf_range(-timer/5,timer/5))
-	$backCloud.position += Vector2(0,-0.05*timer) + tempBack
+	$back_cloud.position += Vector2(0,-0.05*timer) + tempBack
 
-	$frontCloud.position -= tempFront
+	$front_cloud.position -= tempFront
 	tempFront = Vector2(randf_range(-timer/4,timer/4)*randf_range(-timer/4,timer/4),randf_range(-timer/4,timer/4)*randf_range(-timer/4,timer/4))
-	$frontCloud.position += Vector2(0,-0.035*timer) + tempFront
+	$front_cloud.position += Vector2(0,-0.035*timer) + tempFront
