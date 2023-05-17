f5df5d11992021d7e23b07aff831a55adb950c86
John Meadow <armitage3@gazeta.pl>
2023-05-17 19:07:35 +0200

    added F1 open user data folder

--- source/scenes/main_menu/main_menu.gd
+++ source/scenes/main_menu/main_menu.gd
@@ -15,3 +15,8 @@ func _on_TextureRect_resized():
 func _physics_process(_delta):
 	if Input.is_action_just_released("ui_select"):
 		get_tree().change_scene_to_file("res://scenes/game/main.tscn")
+		
+func _input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_F1:
+		OS.shell_open(ProjectSettings.globalize_path("user://"))
+		
