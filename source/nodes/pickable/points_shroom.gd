28d63ec4aa464aca795bf84d3cb666932018666f
John Meadow <armitage3@gazeta.pl>
2023-05-17 19:10:40 +0200

    added shroom AOI

--- source/nodes/pickable/points_shroom.gd
+++ source/nodes/pickable/points_shroom.gd
@@ -1,26 +1,84 @@
 extends Node3D
 
-var pickable        = false
-var size            = 5.0
-var max_size        = 20
-var growth_rate     = 0.6
-var shroom_material = [preload("res://nodes/pickable/model/points_shroom_3.material"),preload("res://nodes/pickable/model/points_shroom_4.material") ]
+var pickable        := false
+var max_size        := 20.0
+var growth_rate     := 0.6
+var shroom_material := [preload("res://nodes/pickable/model/points_shroom_3.material"),preload("res://nodes/pickable/model/points_shroom_4.material") ]
+var b_box_points  := PackedVector3Array([Vector3(1,2,0), Vector3(-1,2,0),Vector3(1,-2,0), Vector3(-1,-2,0)])
+var is_on_screen := false
+
+var aoi_count := 0
+var aoi := {
+	"Blue": 0,
+	"Green": 255,
+	"Red": 0,
+#	"Tags": [],
+	"Name": "Shroom",
+	"KeyFrames": [
+#		{
+#			"IsActive": false,
+#			"Seconds": 0.0,
+#			"Vertices": [
+#				{"X": 0.0, "Y": 0.0},
+#				{"X": 1.0, "Y": 0.0},
+#				{"X": 1.0, "Y": 1.0},
+#				{"X": 0.0, "Y": 1.0}
+#			]
+#		}
+	]
+}
+@onready var mesh = $MeshInstance3D
 
 func _ready():
-	$MeshInstance3D.rotation.y        = randf_range(0,360)
-#	$MeshInstance3D.set_surface_override_material(0, shroom_material[randi()%2])
-	$MeshInstance3D.material_override = shroom_material[randi()%2]
+	mesh.rotation.y        = randf_range(0,360)
+	mesh.material_override = shroom_material[randi()%2]
 	%groans.play()
-	set_physics_process(false)
+#	set_physics_process(false)
 	
 func _physics_process(delta):
 	logging.add_shroom_position( Vector2(global_position.x,global_position.z) )
-	if size < max_size:
-		size += growth_rate * delta
-		$MeshInstance3D.scale = Vector3(size,size,size)
-	else:
-		set_physics_process(false)
-		
+	if mesh.scale.x < max_size:
+		mesh.scale += Vector3.ONE * growth_rate * delta
+#	else:
+#		set_physics_process(false)
+	if is_on_screen:
+		update_on_screen_rect()
+
+func update_on_screen_rect():
+	var bounding_rect:Rect2 = Rect2()
+	for camera_id in globals.cameras.size():
+
+		bounding_rect = Rect2() 
+		bounding_rect.position = globals.cameras[camera_id].unproject_position(global_position) 
+		# BBOX na Viewport coords
+		for i in b_box_points.size():
+			bounding_rect = bounding_rect.expand(globals.cameras[camera_id].unproject_position(to_global(b_box_points[i])))
+		var vieport_size = get_viewport().size * 0.5
+		var vieport_rect = Rect2(Vector2.ZERO, vieport_size)
+		bounding_rect = bounding_rect.intersection(vieport_rect)
+		var vieport_offset = Vector2( (floori(camera_id/2.0))%2, camera_id%2)
+
+		bounding_rect.position += vieport_offset * vieport_size
+		if bounding_rect.size.length():
+			%on_screen_debug._update_rect_for_camera(bounding_rect, camera_id)
+			add_aoi(bounding_rect)
+
+func add_aoi(bbox:Rect2) ->void :
+	bbox.position = bbox.position.round()
+	bbox.size = bbox.size.round()
+	var new_aoi = {
+			"IsActive": true,
+			"Seconds": logging.frame_time_sec,
+			"Vertices": [
+				{"X": bbox.position.x, "Y": bbox.position.y},
+				{"X": bbox.position.x + bbox.size.x, "Y": bbox.position.y},
+				{"X": bbox.position.x + bbox.size.x, "Y": bbox.position.y + bbox.size.y},
+				{"X": bbox.position.x, "Y": bbox.position.y + bbox.size.y}
+			]
+		}
+	aoi.KeyFrames.append(new_aoi)
+	aoi_count += 1
+
 func _on_Area_body_entered(body):
 	if body.is_in_group("players"):
 		pickable = true
@@ -33,3 +91,15 @@ func _on_Area_body_exited(body):
 			pass
 		else: 
 			pickable = false
+
+func _on_visible_on_screen_notifier_3d_screen_entered():
+	is_on_screen = true
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	is_on_screen = false
+
+func _exit_tree():
+	if aoi_count > 0:
+		logging.add_aoi(aoi)
+	
+
