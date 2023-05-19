0aa2aabf0ace8bd1386473e43678c29508379462
John Meadow <leszek.nowak@uj.edu.pl>
2023-05-20 01:11:31 +0200

    cleanup

--- source/resources/globals.gd
+++ source/resources/globals.gd
@@ -6,7 +6,6 @@ var player_score       := [0,0,0,0]
 var player_score_label := [null,null,null,null]
 var player_label       := ["Red", "Green", "Violet", "Blue"]
 var player_count       := 2
-var players_enabled    := [true,false,false,false]
 var victory_label      := "no"
 #var blinder            := null
 #var winnerLabel        := null
